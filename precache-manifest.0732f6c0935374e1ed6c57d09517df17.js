0497f4bcd034900a03f0abe6a6b681aabd6feb67
aeuna <4294psy@naver.com>
2020-06-18 17:20:23 +0900

    Updates

---
+++ precache-manifest.0732f6c0935374e1ed6c57d09517df17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4019a0685a1eacb0e77d96f2b1bffc6e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "ab40930bfd4b56e24064",
+    "url": "/movie_app_2020/static/css/main.92659422.chunk.css"
+  },
+  {
+    "revision": "81f88c63ad6efdfb0d6b",
+    "url": "/movie_app_2020/static/js/2.d9b12856.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.d9b12856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab40930bfd4b56e24064",
+    "url": "/movie_app_2020/static/js/main.8097e3f2.chunk.js"
+  },
+  {
+    "revision": "d5ba99470fbd5d5276a4",
+    "url": "/movie_app_2020/static/js/runtime-main.6a93e83b.js"
+  }
+]);
