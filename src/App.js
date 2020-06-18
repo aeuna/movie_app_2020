e2a3f714b7eedb4637d16cfc0f7e091da35df01e
aeuna <4294psy@naver.com>
2020-06-18 14:59:28 +0900

    07 영화앱 다듬기

--- src/App.js
+++ src/App.js
@@ -22,12 +22,12 @@ class App extends React.Component {
   }
   render() {
     const { isLoading, movies } = this.state
-    return (<section class="container">{isLoading ? (
-      <div class="loader">
-        <span class="loader__text">Loading...</span>
+    return (<section className="container">{isLoading ? (
+      <div className="loader">
+        <span className="loader__text">Loading...</span>
       </div>
     ) : (
-        <div class="movies">
+        <div className="movies">
           {
             movies.map(movie => (
               <Movie
@@ -37,6 +37,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
         </div>
