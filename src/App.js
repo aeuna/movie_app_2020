30f2d1ea5b534c803360826cafa942ce5b7acf84
aeuna <4294psy@naver.com>
2020-06-18 14:38:42 +0900

    06 영화앱 뼈대 만들기

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie'
+import './App.css';
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    console.log('hello')
-  }
-  componentDidMount() {
-    console.log('component renderd')
-  }
-
-  componentDidUpdate() {
-    console.log('I just updated')
-  }
-  componentWillMount() {
-    console.log('Goodbye, cruel world');
-  }
   state = {
-    count : 0,
+    isLoading: true,
+    movies: [],
   };
-
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
+  getMovies = async () => { //자바스크립트에게 getMovies()함수는 시간이 필요하다 알려줌
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating'); //axios.get()의 실행을 기다려 달라고 말해준다
+    this.setState({ movies, isLoading: false })
   }
-
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
+  componentDidMount() { //App component가 그려지면 호출되는 함수
+    //영화 데이터 로딩
+    this.getMovies();
   }
-
-  render(){
-    console.log("I'm rendering")
-    return(
-      <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+  render() {
+    const { isLoading, movies } = this.state
+    return (<section class="container">{isLoading ? (
+      <div class="loader">
+        <span class="loader__text">Loading...</span>
       </div>
-    )
+    ) : (
+        <div class="movies">
+          {
+            movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+        </div>
+      )}
+    </section>
+    )//로딩 완료시 영화데이터 출력
   }
 }
 
